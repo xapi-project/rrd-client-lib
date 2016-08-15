a6d17163e76ba5ceafb2b15a8f88aeb2a2b135e2
Christian Lindig <christian.lindig@citrix.com>
2016-08-15 10:23:58 +0100

    CP-18405 add comments to travis-ci.sh to explain use of init-container.sh
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- travis-ci.sh
+++ travis-ci.sh
@@ -5,6 +5,9 @@
 #
 set -e
 
+# init-container.sh executes either COMMAND or opens a login shell.
+# We just want it to do the initialisation and so we provide a dummy
+# command and do everything else explicitly here.
 env COMMAND=":" /usr/local/bin/init-container.sh
 sudo yum install -y ocaml-rrd-transport-devel
 cd /mnt
